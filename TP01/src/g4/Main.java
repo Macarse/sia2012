43079be8481a24626b39a068b5839098ece65558
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-03-15 17:42:18 +0000

    isValid() now working correctly
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@17 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

--- TP01/src/g4/Main.java
+++ TP01/src/g4/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
   public static void main(String[] args) {
     G4GPSEngine engine = new G4GPSEngine();
-    engine.engine(new Layout4Problem(), SearchStrategy.IterativeDepth);
+    engine.engine(new Layout4Problem(), SearchStrategy.DFS);
   }
 
 }
