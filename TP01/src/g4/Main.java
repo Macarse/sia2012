e16d1541e59f4f7f2bdbb525ce1e3c9bec4b71e6
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-03-15 18:36:27 +0000

    merged
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@34 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

--- TP01/src/g4/Main.java
+++ TP01/src/g4/Main.java
@@ -10,36 +10,45 @@ import org.kohsuke.args4j.CmdLineParser;
 
 public class Main {
 
-	
-  public static void main(String[] args) {
-	  
+
+	public static void main(String[] args) {
+
 		CommandLineParser arguments = new CommandLineParser();
-	    CmdLineParser parser = new CmdLineParser(arguments);
-	    try {
+		CmdLineParser parser = new CmdLineParser(arguments);
+		try {
 			parser.parseArgument(args);
 		} catch (CmdLineException e) {
 			System.err.println("Parametros invalidos");
 			parser.printUsage(System.err);
 			System.exit(1);
 		}
-		
+
 		if (arguments.help) {
 			parser.printUsage(System.out);
 			System.exit(1);
 		}
-		
+
 		if(arguments.getStrategy() == null) {
 			System.err.println("Es necesario ingresar una estrategia de busqueda");
 			parser.printUsage(System.err);
 			System.exit(1);
 		}
 		SearchStrategy strategy = arguments.getStrategy();
-		
+
 		GPSProblem problem = ProblemFactory.createProblem(arguments.getLevel());
-		
-	  
-    G4GPSEngine engine = new G4GPSEngine();
-    engine.engine(problem, strategy);
-  }
+
+		if(problem == null){
+			System.err.println("Invalid problem level");
+			System.exit(1);
+		}
+		if(strategy == null){
+			System.err.println("Invalid Strategy");
+			System.exit(1);
+
+		}
+
+		G4GPSEngine engine = new G4GPSEngine();
+		engine.engine(problem, strategy);
+	}
 
 }
--- TP01/src/g4/parseLine/CommandLineParser.java
+++ TP01/src/g4/parseLine/CommandLineParser.java
@@ -6,13 +6,11 @@ import org.kohsuke.args4j.Option;
 
 public class CommandLineParser {
 
-	@Option(name="-s", aliases={"--strategy"}, usage="Select the Strategy to solve [BFS | DFS | AStar | IterativeDepth | Greedy]")
+	@Option(name="-s", aliases={"--strategy"}, usage="Select the Strategy to solve [BFS | DFS | IterativeDepth ]")
 	private SearchStrategy strategies;
 	
-//	@Option(name="-h", aliases={"--heuristic"}, usage="Select the heuristics [EmptySpaces | EmptySpaces_for_turns | Grades_Sum | Amount_of_Walkable_Cells | Unreachable_space | Cell_Grade_Evaluation | Costh | Cols_Filled | Rows_Filled | Starting_Point]")
-//	private List<String> heuristics;
 
-	@Option(name="-l", aliases={"--level"}, usage="Select the game you want to play[ 1 | 2 | 3 | 4 | 5 | -1]")
+	@Option(name="-l", aliases={"--level"}, usage="Select the game you want to play[ 1 | 2 | 3 | 4 ]")
 	private int level;
 	
 	private boolean hasToDraw = false;
@@ -23,25 +21,6 @@ public class CommandLineParser {
 		return strategies;
 	}
 
-//	public List<GPSHeuristic> getHeuristics() {
-//		List<GPSHeuristic> heuristics = Lists.newArrayList();
-//		if(this.heuristics != null) {
-//			for (String gpsHeuristic : this.heuristics) {
-//				WPSHeuristic h = WPSHeuristic.fromString(gpsHeuristic);
-//				if (h != null) heuristics.add(HeurisitcFactory.getHeuristic(h));
-//			}
-//		}
-//		return heuristics;
-//	}
-//
-//	public WPSLevel getLevel() {
-//		return WPSLevel.fromInteger(level);
-//	}
-	
-//	@Option(name="-d", aliases={"--drawing"}, usage="Choose if you want to draw the board")
-//	public void setDrawing(boolean flag){
-//		hasToDraw = flag;
-//	}
 	
 	public int getLevel(){
 		return level;
