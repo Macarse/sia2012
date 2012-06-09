1551208eff8487aa94d1d0f0d21a3f8b2bc9013d
Nicolas Purita <nicospuri@gmail.com>
2012-06-09 20:34:24 -0300

    Finish algorithm

--- TP04/java/src/com/g4/java/crossover/SinglePointCrossOver.java
+++ TP04/java/src/com/g4/java/crossover/SinglePointCrossOver.java
@@ -8,9 +8,27 @@ public class SinglePointCrossOver {
   public static Individual[] cross(Individual[] entities) {
     double[] dad = entities[0].getLupusArray();
     double[] mom = entities[1].getLupusArray();
-
+    double[] son1 = new double[dad.length];
+    double[] son2 = new double[dad.length];
+    
+    
     int cutPoint = RandomGenerator.getInt(1, dad.length);
     
-    return null;
+    
+    for (int i = 0; i < cutPoint; i++) {
+    	son1[i] = dad[i];
+    	son2[i] = mom[i];
+	}
+    
+    for (int i = cutPoint; i < mom.length; i++) {
+    	son2[i] = dad[i];
+    	son1[i] = mom[i];
+	}
+    
+    Individual indSon1 = Individual.creator(entities[0].getData(), son1);
+    Individual indSon2 = Individual.creator(entities[0].getData(), son2);
+    
+    return new Individual[]{indSon1, indSon2};
   }
+  
 }
---
+++ TP04/java/src/com/g4/java/mutation/ClassicMutation.java
@@ -0,0 +1,34 @@
+package com.g4.java.mutation;
+
+import com.g4.java.model.Individual;
+import com.g4.java.util.RandomGenerator;
+
+public class ClassicMutation {
+
+	protected double mutationPercentage;
+
+	public ClassicMutation(double mutationPercentage) {
+		this.mutationPercentage = mutationPercentage;
+	}
+
+	public Individual mutate(Individual entity, int iteration) {
+		double[] bits = entity.getLupusArray();
+		for (int i = 0; i < bits.length; ++i) {
+			double charProba = Math.random();
+			if (charProba < mutationPercentage * 2) {
+				bits[i] = RandomGenerator.getDouble(-2, 2);
+			}
+		}
+		return Individual.creator(entity.getData(), bits);
+	}
+
+	public double getMutationProbability() {
+		return mutationPercentage;
+	}
+
+	public boolean shouldMutate() {
+		double prob = RandomGenerator.getDouble();
+		return prob < mutationPercentage;
+	}
+
+}
