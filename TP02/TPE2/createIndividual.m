ac5e9bd514f924668ee5a659b2e072d7c8558067
Nicolas Purita <nicospuri@gmail.com>
2012-06-10 00:36:59 -0300

    Modified matlab functions

--- TP02/TPE2/createIndividual.m
+++ TP02/TPE2/createIndividual.m
@@ -1,55 +1,18 @@
-function [ allWeights ] = createIndividual(inputs, expected_outputs, inputs_test, expected_outputs_test)
+function [ allWeights ] = createIndividual(inputs, expected_outputs, inputs_test, expected_outputs_test, index)
 
-N = 2;
+	conf = globalConf;
 
-neurons_per_layer = [30 20 10 1];
+	params = conf.getParams();
+	eta = params.eta;
+	alpha = params.alpha;
+	momentumIsEnabled = params.momentumIsEnabled;	
 
-eta = 0.4;
-
-epsilon  = 0.01;
-
-g = @no_lineal_exp;
-
-g_deriv = @no_lineal_deriv_exp;
-
-lineal_function = @lineal;
-
-lineal_function_deriv = @lineal_deriv;
-
-M = 351;
-
-etaDecrement = 0.01;
-
-etaIncrement = 0.1;
-
-k = 5;
-
-etaAdapIsEnabled = 1;
-
-momentumIsEnabled = 1;
-
-func = @evaluation_function;
-
-func_deriv = @evaluation_function_deriv;
-
-alpha = 0.2;
-neurons_per_layer = [N neurons_per_layer];
-
-numberOfLayers = length(neurons_per_layer(1,:))-1;
-
-allWeights = generateWeights(neurons_per_layer);
-
-activationFunctions = cell(2, numberOfLayers);
-activationFunctions(1,numberOfLayers) = {lineal_function};
-activationFunctions(2,numberOfLayers) = {lineal_function_deriv};
-
-for i = 1:numberOfLayers-1
-   activationFunctions(1,i) = {g};
-   activationFunctions(2,i) = {g_deriv};
-end
+	neurons_per_layer = conf.getConfiguration(index);
+	allWeights = generateWeights(neurons_per_layer);
+	numberOfLayers = length(neurons_per_layer(1,:))-1;
+	activationFunctions = conf.getActivationFunction(numberOfLayers);
 
 
-allWeights = train( activationFunctions(1,:), activationFunctions(2,:), allWeights, inputs, expected_outputs, eta, momentumIsEnabled, alpha );
-
+	allWeights = train( activationFunctions(1,:), activationFunctions(2,:), allWeights, inputs, expected_outputs, eta, momentumIsEnabled, alpha );
 
 end
