c16356898d9dc0beee7a0f637b5bbcbbcefd0992
Nicolas Purita <nicospuri@gmail.com>
2012-06-10 15:08:29 -0300

    Modified Universal

--- TP04/java/src/com/g4/java/selection/UniversalSelection.java
+++ TP04/java/src/com/g4/java/selection/UniversalSelection.java
@@ -1,47 +1,34 @@
 package com.g4.java.selection;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.g4.java.model.Individual;
-import com.g4.java.util.Pair;
+import com.g4.java.util.RandomGenerator;
 
 public class UniversalSelection implements Selection {
 
 	@Override
 	public List<Individual> select(List<Individual> population, int toSelect) {
-		List<Pair<Individual, Double>> aptitudes = new ArrayList<Pair<Individual,Double>>();
-		
-		// Total Fitness of population
-		int sum = 0;
-		for (Individual entity : population) {
-			double ap = entity.getApptitude();
-			sum += ap;
-			aptitudes.add(new Pair<Individual, Double>(entity, ap));
-		}
-		
-		
-		List<Double> randoms = new ArrayList<Double>();
-		double r = Math.random();
-		for(int i = 0 ; i < toSelect ; ++i){
-			double rj = (r + i) / toSelect;
-			rj -= Math.floor(rj);
-			randoms.add(rj);
-		}
-		
-		Collections.sort(randoms);
-		List<Individual> selecteds = new ArrayList<Individual>();
-		
-		double acum = 0;
-		for(int i = 0, j = 0; i < toSelect ; ++i){
-			while( j < aptitudes.size() && randoms.get(i) > acum+(aptitudes.get(j).getO2()/sum)){
-				acum += aptitudes.get(j).getO2()/sum;
-				j++;
-			}
-			selecteds.add(aptitudes.get(j).getO1());
-		}
-		return selecteds;
+		List<Individual> newGeneration = new ArrayList<Individual>();
+
+        double distance = 1.0d / toSelect;
+        double r = RandomGenerator.getDouble() / toSelect;
+        
+        for (int i = 0; i < toSelect; i++) {
+            double f = 0;
+            int j = 0;
+            Individual individual = null;
+            
+            do {
+                individual = population.get(j++);
+                f += individual.getApptitude();
+            } while (f < r);
+            newGeneration.add(individual);
+            r += distance;
+        }
+
+        return newGeneration;
 
 	}
 
