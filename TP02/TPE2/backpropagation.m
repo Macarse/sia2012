5a4f718b1c6d533fa0038cea30d0886d22e71776
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-06-12 16:29:02 +0000

    Add backpropagation function
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@50 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

---
+++ TP02/TPE2/backpropagation.m
@@ -0,0 +1,59 @@
+%% backpropagation: function description
+function [outputs] = backpropagation(allWeights, inputs, expected_outputs, iterations)
+	N = 2;
+
+	neurons_per_layer = [30 20 10 1];
+
+	eta = 0.4;
+
+	epsilon  = 0.01;
+
+	g = @no_lineal_exp;
+
+	g_deriv = @no_lineal_deriv_exp;
+
+	lineal_function = @lineal;
+
+	lineal_function_deriv = @lineal_deriv;
+
+	M = 351;
+
+	etaDecrement = 0.01;
+
+	etaIncrement = 0.1;
+
+	k = 5;
+
+	etaAdapIsEnabled = 1;
+
+	momentumIsEnabled = 1;
+
+	func = @evaluation_function;
+
+	func_deriv = @evaluation_function_deriv;
+
+	alpha = 0.2;
+	neurons_per_layer = [N neurons_per_layer];
+
+	numberOfLayers = length(neurons_per_layer(1,:))-1;
+
+	allWeights = generateWeights(neurons_per_layer);
+
+	activationFunctions = cell(2, numberOfLayers);
+	activationFunctions(1,numberOfLayers) = {lineal_function};
+	activationFunctions(2,numberOfLayers) = {lineal_function_deriv};
+
+	for i = 1:numberOfLayers-1
+	   activationFunctions(1,i) = {g};
+	   activationFunctions(2,i) = {g_deriv};
+	end
+
+	rounds = 0;
+	while rounds < iterations
+		allWeights = train( activationFunctions(1,:), activationFunctions(2,:), allWeights, inputs, expected_outputs, eta, momentumIsEnabled, alpha );
+		rounds = rounds + 1;
+	end
+
+	outputs = allWeights;
+
+end
