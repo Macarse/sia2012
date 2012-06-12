960242d9291075c689ec575404b1f875684f916a
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-06-12 16:28:27 +0000

    Finish algorithm
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@49 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

--- TP04/java/src/com/g4/java/FunctionResolver.java
+++ TP04/java/src/com/g4/java/FunctionResolver.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.g4.java.crossover.SinglePointCrossOver;
 import com.g4.java.model.Individual;
+import com.g4.java.mutation.ClassicMutation;
 import com.g4.java.reproduction.MonogamousReproduction;
 import com.g4.java.selection.EliteSelection;
 import com.g4.java.util.InputValues;
@@ -15,77 +16,103 @@ import com.mathworks.toolbox.javabuilder.MWException;
 
 public class FunctionResolver {
 
-  private static final int POP_SIZE = 20;
-  private static final int MAX_GENERATIONS = 10;
+	private static final int POP_SIZE = 50;
+	private static final int MAX_GENERATIONS = 100;
 
-  private List<Individual> population = new ArrayList<Individual>(POP_SIZE);
+	private List<Individual> population = new ArrayList<Individual>(POP_SIZE);
 
-  public static void main(String[] args) {
-    FunctionResolver functionResolver = new FunctionResolver();
+	public static void main(String[] args) {
+		FunctionResolver functionResolver = new FunctionResolver();
 
-    try {
-      functionResolver.run();
-    } catch (MWException e) {
-      System.err.println(e.getCause());
-      System.exit(0);
-    }
-  }
-  
-  private void run() throws MWException {
-    ANN ann = null;
+		try {
+			functionResolver.run();
+		} catch (MWException e) {
+			System.err.println(e.getCause());
+			System.exit(0);
+		}
+	}
 
-    // Init the matlab code.
-      ann = new ANN();
+	private void run() throws MWException {
+		ANN ann = null;
 
-    // Create the input values based on samples3.txt
-    Object[] inputResult = ann.generateInputFromFile(4, "samples3.txt", .80f, 0);
-    InputValues.getInstance().setInputs((MWArray) inputResult[0]);
-    InputValues.getInstance().setExpectedOutputs((MWArray) inputResult[1]);
-    InputValues.getInstance().setInputsTest((MWArray) inputResult[2]);
-    InputValues.getInstance().setExpectedOutputsTest((MWArray) inputResult[3]);
+		// Init the matlab code.
+		ann = new ANN();
 
-    // Create the evaluation function.
-    Function function = new Function(ann);
-    long creationStartTime = System.currentTimeMillis();
+		// Create the input values based on samples3.txt
+		Object[] inputResult = ann.generateInputFromFile(4, "samples3.txt",
+				.80f, 0);
+		InputValues.getInstance().setInputs((MWArray) inputResult[0]);
+		InputValues.getInstance().setExpectedOutputs((MWArray) inputResult[1]);
+		InputValues.getInstance().setInputsTest((MWArray) inputResult[2]);
+		InputValues.getInstance().setExpectedOutputsTest(
+				(MWArray) inputResult[3]);
 
-    // Create the initial population
-    for (int i = 0; i < POP_SIZE; i++) {
-      Individual individual = new Individual();
-      Object[] createIndividualResult = ann.createIndividual(1,
-          InputValues.getInstance().getInputs(),
-          InputValues.getInstance().getExpectedOutputs(),
-          InputValues.getInstance().getInputsTest(),
-          InputValues.getInstance().getExpectedOutputsTest());
+		// Create the evaluation function.
+		Function function = new Function(ann);
+		long creationStartTime = System.currentTimeMillis();
 
-      individual.setData((MWCellArray) createIndividualResult[0]);
-      population.add(individual);
-    }
+		// Create the initial population
+		for (int i = 0; i < POP_SIZE; i++) {
+			Individual individual = new Individual();
+			Object[] createIndividualResult = ann.createIndividual(1,
+					InputValues.getInstance().getInputs(), InputValues
+							.getInstance().getExpectedOutputs(), InputValues
+							.getInstance().getInputsTest(), InputValues
+							.getInstance().getExpectedOutputsTest());
 
-    // Calculate the aptitude for every individual.
-    for (int i = 0 ; i < POP_SIZE ; i++ ) {
-      population.get(i).setApptitude(function.eval(population.get(i)));
-    }
+			individual.setData((MWCellArray) createIndividualResult[0]);
+			population.add(individual);
+		}
 
-    System.out.println("Whole creation process took: " + (System.currentTimeMillis() - creationStartTime));
+		// Calculate the aptitude for every individual.
+		for (int i = 0; i < POP_SIZE; i++) {
+			population.get(i).setApptitude(function.eval(population.get(i)));
+		}
 
-    EliteSelection selection = new EliteSelection();
+		System.out.println("Whole creation process took: "
+				+ (System.currentTimeMillis() - creationStartTime));
 
-    for(int i = 0 ; i < MAX_GENERATIONS ; ++i) {
-      List<Individual> best = selection.select(population, POP_SIZE / 2);
-      List<Individual[]> parents = MonogamousReproduction.getParents(best);
-      List<Individual> generation = new ArrayList<Individual>();
-      List<Individual> sons = new ArrayList<Individual>();
-      for (Individual[] family : parents) {
-        generation.add(family[0]);
-        generation.add(family[1]);
+		EliteSelection selection = new EliteSelection();
+		ClassicMutation mutation = new ClassicMutation(0.1);
 
-        Individual[] childs = SinglePointCrossOver.cross(family);
-        sons.add(childs[0]);
-        sons.add(childs[1]);
-      }
-      
-      
-    }
-    
-  }
+		for (int i = 0; i < MAX_GENERATIONS; ++i) {
+			List<Individual> best = selection.select(population, POP_SIZE / 2);
+			List<Individual[]> parents = MonogamousReproduction
+					.getParents(best);
+			List<Individual> generation = new ArrayList<Individual>();
+			List<Individual> sons = new ArrayList<Individual>();
+
+			for (Individual[] family : parents) {
+				generation.add(family[0]);
+				generation.add(family[1]);
+
+				Individual[] childs = SinglePointCrossOver.cross(family);
+				sons.add(childs[0]);
+				sons.add(childs[1]);
+			}
+
+			List<Individual> sonsToAdd = new ArrayList<Individual>();
+			for (Individual individual : sons) {
+				if (mutation.shouldMutate()) {
+					sonsToAdd.add(mutation.mutate(individual, i));
+				} else {
+					sonsToAdd.add(individual);
+				}
+			}
+			sons.clear();
+			sons = null;
+
+			generation.addAll(sonsToAdd);
+			
+			for (Individual individual : sonsToAdd) {
+				individual.setApptitude(function.eval(individual));
+			}
+			
+			population = selection.select(generation, POP_SIZE);
+			System.out.println("Best individual (Apptitude) " + population.get(0).getApptitude());
+			System.out.println("Worst individual (Apptitude) " + population.get(POP_SIZE-1).getApptitude());
+			System.out.println("Finish Generation " + i);
+		}
+
+	}
 }
--- TP04/java/src/com/g4/java/util/RandomGenerator.java
+++ TP04/java/src/com/g4/java/util/RandomGenerator.java
@@ -4,29 +4,33 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-  public static Random random = new Random();
+	public static Random random = new Random();
 
-  public static int getInt(int lowerBound, int upperBound) {
-    if (lowerBound == upperBound) {
-      return lowerBound;
-    }
-    return random.nextInt(upperBound - lowerBound) + lowerBound;
-  }
+	public static int getInt(int lowerBound, int upperBound) {
+		if (lowerBound == upperBound) {
+			return lowerBound;
+		}
+		return random.nextInt(upperBound - lowerBound) + lowerBound;
+	}
 
-  public static int getInt(int upperBound) {
-    return random.nextInt(upperBound);
-  }
+	public static double getDouble(double lowerBound, double upperBound) {
+		return random.nextDouble() * Math.abs(upperBound - lowerBound) + Math.min(lowerBound, upperBound);
+	}
 
-  public static int getInt() {
-    return random.nextInt();
-  }
+	public static int getInt(int upperBound) {
+		return random.nextInt(upperBound);
+	}
 
-  public static double getDouble() {
-    return random.nextDouble();
-  }
+	public static int getInt() {
+		return random.nextInt();
+	}
 
-  public static boolean getBoolean(double trueProbalility) {
-    return random.nextDouble() < trueProbalility;
-  }
+	public static double getDouble() {
+		return random.nextDouble();
+	}
+
+	public static boolean getBoolean(double trueProbalility) {
+		return random.nextDouble() < trueProbalility;
+	}
 
 }
