079be14fcba9b806e3394f2b7ca1d005e273e87f
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-06-12 16:34:06 +0000

    Fix bug
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@80 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

--- TP04/java/src/com/g4/java/reproduction/MonogamousReproduction.java
+++ TP04/java/src/com/g4/java/reproduction/MonogamousReproduction.java
@@ -10,6 +10,11 @@ public class MonogamousReproduction implements Reproduction {
   public List<Individual[]> getParents(List<Individual> populations) {
     int size = populations.size();
 
+    if ( size % 2 != 0 ) {
+      populations.add(populations.get(size-1));
+      size++;
+    }
+
     List<Individual[]> families = new ArrayList<Individual[]>(size * 2);
 
     for (int i = 0; i < size / 2; ++i) {
