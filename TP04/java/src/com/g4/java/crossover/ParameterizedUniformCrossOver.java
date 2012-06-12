4192048a94042e485839dd27741b46f1c49f6162
csessa <csessa@39a6b423-ae9d-42ef-8f9a-67f0a12ae516>
2012-06-12 16:32:37 +0000

    ParameterizedUniformCrossOver
    git-svn-id: https://repo.it.itba.edu.ar/svn/sia-2012-4@62 39a6b423-ae9d-42ef-8f9a-67f0a12ae516

---
+++ TP04/java/src/com/g4/java/crossover/ParameterizedUniformCrossOver.java
@@ -0,0 +1,42 @@
+package com.g4.java.crossover;
+
+import com.g4.java.model.Individual;
+import com.g4.java.util.RandomGenerator;
+
+public class ParameterizedUniformCrossOver implements Crossover {
+
+  private double prob;
+  
+  public ParameterizedUniformCrossOver(double prob) {
+    prob = this.prob;
+  }
+
+  @Override
+  public Individual[] cross(Individual[] entities) {
+    double[] dad = entities[0].getLupusArray();
+    double[] mom = entities[1].getLupusArray();
+    double[] son1 = new double[dad.length];
+    double[] son2 = new double[dad.length];
+
+    for (int i = 0; i < dad.length; i++) {
+      son1[i] = dad[i];
+      son2[i] = mom[i];
+    }
+
+    for (int i = 0; i < dad.length; i++) {
+      if ( RandomGenerator.getDouble() < this.prob ) {
+        double aux = son1[i];
+        son1[i] = son2[i];
+        son2[i] = aux;
+      }
+    }
+
+    Individual indSon1 = Individual
+        .creator(entities[0].getData(), son1);
+    Individual indSon2 = Individual
+        .creator(entities[0].getData(), son2);
+
+    return new Individual[] { indSon1, indSon2 };
+  }
+
+}
